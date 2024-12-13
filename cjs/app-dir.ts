5f23c68578c88cd232f485632944318ab2b1489a
Baptiste Pillot <baptiste@pillot.fr>
2024-12-13 16:29:15 +0100

    Fix issue with functionality in subdirectory setup

--- cjs/app-dir.ts
+++ cjs/app-dir.ts
@@ -1,5 +1,5 @@
 import { existsSync } from 'node:fs'
 import { normalize }  from 'node:path'
 
-const appDir = normalize(__dirname + (existsSync(__dirname + '/../../../node_modules') ? '/../../..' : '/..'))
+const appDir = normalize(__dirname + (existsSync(__dirname + '/../../../../node_modules') ? '/../../../..' : '/..'))
 export default appDir
--- esm/app-dir.ts
+++ esm/app-dir.ts
@@ -3,5 +3,5 @@ import { dirname, normalize } from 'node:path'
 import { fileURLToPath }      from 'node:url'
 
 const dirName = dirname(fileURLToPath(import.meta.url))
-const appDir = normalize(dirName + (existsSync(dirName + '/../../../node_modules') ? '/../../..' : '/..'))
+const appDir = normalize(dirName + (existsSync(dirName + '/../../../../node_modules') ? '/../../../..' : '/..'))
 export default appDir
