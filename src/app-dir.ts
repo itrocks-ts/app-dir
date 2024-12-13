252b97a7528988169abb9ad3f568bc7fb5dbe05e
Baptiste Pillot <baptiste@pillot.fr>
2024-12-13 12:01:44 +0100

    Fix dual Node.js compatibility for ESM and CommonJS at runtime

--- src/app-dir.ts
+++ src/app-dir.ts
@@ -1,5 +1,10 @@
 import { existsSync } from 'node:fs'
-import { normalize }  from 'node:path'
+import { dirname, normalize }  from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+// @ts-ignore Compatibility code
+__filename ??= fileURLToPath(import.meta.url)
+__dirname  ??= dirname(__filename)
 
 const appDir = normalize(__dirname + (existsSync(__dirname + '/../../../node_modules') ? '/../../..' : '/..'))
 export default appDir
