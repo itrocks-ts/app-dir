33be32de2057393b5dbef0cb990a891694d8532f
Baptiste Pillot <baptiste@pillot.fr>
2026-05-08 19:05:32 +0200

    Upgrade typescript to version 6.0

--- esm/app-dir.ts
+++ esm/app-dir.ts
@@ -1,6 +1,7 @@
-import { existsSync }         from 'node:fs'
-import { dirname, normalize } from 'node:path'
-import { fileURLToPath }      from 'node:url'
+import { existsSync }    from 'node:fs'
+import { dirname }       from 'node:path'
+import { normalize }     from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const dirName = dirname(fileURLToPath(import.meta.url))
 export const appDir = normalize(dirName + (existsSync(dirName + '/../../../../node_modules') ? '/../../../..' : '/..'))
